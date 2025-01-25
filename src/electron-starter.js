647334676e55643b690a036717f495ab79953762
natanfds <natanfernandesdossantos@zohomail.com>
2025-01-25 00:47:16 -0300

    [feat] Permitindo react rodar com arquivos estáticos no electron

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -1,14 +1,30 @@
 const electron = require('electron');
+require('dotenv').config()
+const url = require('url')
+const path = require("path")
+
+
+
 
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
 let mainWindow;
 
+
 function createWindow() {
     mainWindow = new BrowserWindow({width: 800, height: 600});
-    mainWindow.loadURL('http://localhost:3000');
-    mainWindow.webContents.openDevTools();
+
+    const staticFilesURL =  url.format({
+        pathname: path.join(__dirname, '/../build/index.html'),
+        protocol: 'file:',
+        slashes: true
+    });
+    const {ELECTRON_URL} = process.env
+    const initialURL = ELECTRON_URL || staticFilesURL
+    console.log(initialURL)
+    mainWindow.loadURL(initialURL)
+    
     mainWindow.on('closed', function () {
         mainWindow = null
     })
