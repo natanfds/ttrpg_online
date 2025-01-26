16b7159b6273efdeed3241d22b0a5aea7fb7f0c5
natanfds <natanfernandesdossantos@zohomail.com>
2025-01-26 18:34:42 -0300

    [feat] juntando os elementos do chat

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
-import {ChatFeed} from './components/index.tsx'
+import {ChatComponent} from './components/index.tsx'
 
 function App() {
   return (
-    <div>
-      <ChatFeed></ChatFeed>
-    </div>
+      <ChatComponent></ChatComponent>
   );
 }
 
--- src/components/chat/index.tsx
+++ src/components/chat/index.tsx
@@ -1,3 +1,12 @@
-export * from './chatInput.tsx'
-export * from './chatFeed.tsx'
-export * from './messageBubble.tsx'
+import React from "react";
+import { ChatFeed } from "./chatFeed.tsx";
+import { ChatInput } from "./chatInput.tsx";
+
+export const ChatComponent = () => {
+  return (
+    <div className="chat-container">
+      <ChatFeed></ChatFeed>
+      <ChatInput></ChatInput>
+    </div>
+  )
+}
